fec193d4e513ad67bb6b7bced949fdfb2be4fdd1
fazlerabbi2248 <fazlerabbi2248@gmail.com>
2024-01-02 00:29:10 +0600

    Event creation and homepage

---
+++ event/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from event import views
+
+urlpatterns = [
+
+    path('accounts/', include('django.contrib.auth.urls')), # this is build in
+    path('register/', views.register, name='register'), # custom
+    path('login/', views.login, name='login'),
+    path('events/create/', views.create_event, name='create_event'),
+
+]
--- event/views.py
+++ event/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+from .forms import CustomUserCreationForm, EventForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'login.html')
+
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.save()
+            #return redirect('event_list')
+    else:
+        form = EventForm()
+    return render(request, 'create_event.html', {'form': form})
